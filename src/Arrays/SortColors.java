2742db33af1989112de17eb6062c9d66b0a9593b
abhishekjohri <abhishekjohri098@gmail.com>
2025-10-10 16:37:37 +0530

    SortColors

---
+++ src/Arrays/SortColors.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+public class SortColors {
+
+    public static void main(String[] args) {
+
+        int[] nums = {2,0,2,1,1,0};
+        SortColors sortColors = new SortColors();
+        sortColors.sortColors(nums);
+        for(Integer i:nums){
+            System.out.print(i+" ");
+        }
+    }
+
+    public void sortColors(int[] nums) {
+
+        int n = nums.length;
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while(mid <= high){
+
+            if(nums[mid] == 0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else{
+                swap(nums,mid,high);
+                high--;
+            }
+        }
+
+    }
+
+    public static void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
