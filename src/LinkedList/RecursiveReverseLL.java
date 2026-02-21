765b30586f8a9cf1b3e19ac3e0b48839b63cd2fa
abhishekjohri <abhishekjohri098@gmail.com>
2026-02-21 19:07:23 +0530

    RecursiveReverseLL

---
+++ src/LinkedList/RecursiveReverseLL.java
@@ -0,0 +1,26 @@
+package LinkedList;
+
+public class RecursiveReverseLL {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+
+        ListNode front = head.next;
+
+        front.next = head;
+
+        head.next = null;
+
+        return newHead;
+
+    }
+}
