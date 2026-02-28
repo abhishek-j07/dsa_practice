7fe009040f45c47052ed6e6b45bc2d687bc85b47
abhishekjohri <abhishekjohri098@gmail.com>
2026-02-28 14:19:02 +0530

    RemoveNthNodeFromEnd

---
+++ src/LinkedList/RemoveNthNodeFromEnd.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class RemoveNthNodeFromEnd {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode slow = head, fast = head;
+
+        for(int i = 0 ; i < n ; i++){
+            fast = fast.next;
+        }
+
+        if(fast == null){
+            return head.next;
+        }
+
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+    }
+}
