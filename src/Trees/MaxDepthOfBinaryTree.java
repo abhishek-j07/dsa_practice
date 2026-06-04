b93cda48d53b70cf64ca10fc8d3993910c8ea89b
abhishekjohri <abhishekjohri098@gmail.com>
2026-06-04 11:50:50 +0530

    MaxDepthOfBinaryTree

---
+++ src/Trees/MaxDepthOfBinaryTree.java
@@ -0,0 +1,16 @@
+package Trees;
+
+public class MaxDepthOfBinaryTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int maxDepth(InorderTraversal.TreeNode root) {
+
+        if (root == null) return 0;
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+
+    }
+}
